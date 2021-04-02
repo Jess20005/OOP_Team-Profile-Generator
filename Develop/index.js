cb3c2a3ba70b323660336f872fc57900a51de359
Jessica <simply.me05@hotmail.com>
2021-04-02 11:19:53 -0400

    updating index.js and lib folder

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,48 @@
 const inquirer = require("inquirer");
 const fs = require ('fs');
+const Manager = require("./lib/Manager")
+const myTeam = [];
+
+function createEngineer(){
+    //running prompt for engineer questions
+    //using answers from prompt create engineer class
+    //store created engineer in variable
+    //call function for createTeamMember
+}
+
+function createIntern(){
+    //running prompt for intern questions
+    //using answers from prompt create intern class
+    //store created intern in variable
+    //call function for createTeamMember
+}
+
+function buildTeam(){
+    //call writetofile function with variable
+}
 
 const manager = [
     {
     type: 'input',
-    name: 'getName',
+    name: 'managerName',
     message: "What is the team manager's name?",
   },
   {
     type: 'input',
-    name: 'getId',
+    name: 'managerId',
+    message: "What is the team manager's id?",
+  },
+  {
+    type: 'input',
+    name: 'managerEmail',
     message: "What is the team manager's email?",
   },
   {
     type: 'input',
-    name: 'getOfficeNumber',
+    name: 'managerOfficeNumber',
     message: "What is the team manager's office number?",
   },
-  {
-    type: 'list',
-    message: 'What type of team member would you like to add?',
-    name: 'getRole',
-    choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
-  },
+  
   ];
 
   const engineer = [
@@ -34,6 +54,11 @@ const manager = [
   {
     type: 'input',
     name: 'getId',
+    message: "What is the team manager's id?",
+  },
+  {
+    type: 'input',
+    name: 'getEmail',
     message: "What is the team manager's email?",
   },
   {
@@ -59,6 +84,11 @@ const manager = [
   {
     type: 'input',
     name: 'getId',
+    message: "What is the team manager's id?",
+  },
+  {
+    type: 'input',
+    name: 'getEmail',
     message: "What is the team manager's email?",
   },
   {
@@ -73,6 +103,33 @@ const manager = [
     choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
   },
   ];
+  function createManager(){
+    inquirer.prompt(manager)
+    .then(answers => {
+        console.log(answers)
+        const manager = new Manager(
+            answers.managerName,
+            answers.managerId,
+            answers.managerEmail,
+            answers.managerOfficeNumber,
+       )
+       myTeam.push(manager)
+       createTeamMembers()     
+    })
+    //running prompt for manager questions
+    //using answers from prompt to create manager class
+    //store the created manager in variable
+    //call function for team members created
+}
+    createManager()
+
+    
+function createTeamMembers(){
+    //running prompt for team questions
+    //for engineer call create engineer function
+    //for intern call intern function
+    //for done run build team function
+}
 
   // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -84,15 +141,22 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
+
 function init() {
-    inquirer.prompt(manager, engineer, intern)
+    inquirer.prompt(engineer)
     .then(answers => {
         writeToFile('index.html', answers)
         console.log(answers)
     })
 }
 
-// Function call to initialize app
-init();
+function init() {
+    inquirer.prompt(intern)
+    .then(answers => {
+        writeToFile('index.html', answers)
+        console.log(answers)
+    })
+}
+
 
 
