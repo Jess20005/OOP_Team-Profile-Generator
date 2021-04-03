611b17d9998afdeb6bb08e13c952a25b2779f319
Jessica <simply.me05@hotmail.com>
2021-04-02 21:26:25 -0400

    setting up functions for question prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,162 +1,185 @@
 const inquirer = require("inquirer");
-const fs = require ('fs');
-const Manager = require("./lib/Manager")
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 const myTeam = [];
 
-function createEngineer(){
-    //running prompt for engineer questions
-    //using answers from prompt create engineer class
-    //store created engineer in variable
-    //call function for createTeamMember
-}
-
-function createIntern(){
-    //running prompt for intern questions
-    //using answers from prompt create intern class
-    //store created intern in variable
-    //call function for createTeamMember
-}
-
-function buildTeam(){
-    //call writetofile function with variable
-}
-
 const manager = [
-    {
-    type: 'input',
-    name: 'managerName',
+  {
+    type: "input",
+    name: "managerName",
     message: "What is the team manager's name?",
   },
   {
-    type: 'input',
-    name: 'managerId',
+    type: "input",
+    name: "managerId",
     message: "What is the team manager's id?",
   },
   {
-    type: 'input',
-    name: 'managerEmail',
+    type: "input",
+    name: "managerEmail",
     message: "What is the team manager's email?",
   },
   {
-    type: 'input',
-    name: 'managerOfficeNumber',
+    type: "input",
+    name: "managerOfficeNumber",
     message: "What is the team manager's office number?",
   },
-  
-  ];
+];
 
-  const engineer = [
+const engineer = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is your engineer's name?",
+  },
+  {
+    type: "input",
+    name: "engineerId",
+    message: "What is your engineer's id?",
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "What is the your engineer's email?",
+  },
+  {
+    type: "input",
+    name: "engineerGitHub",
+    message: "What is your engineer's Github account?",
+  },
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "internName",
+    message: "What is your intern's name?",
+  },
+  {
+    type: "input",
+    name: "internId",
+    message: "What is your intern's id?",
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "What is the your intern's email?",
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "What is your intern's school?",
+  },
+ 
+];
+
+const employee = [
     {
-    type: 'input',
-    name: 'getName',
-    message: "What is the team manager's name?",
-  },
-  {
-    type: 'input',
-    name: 'getId',
-    message: "What is the team manager's id?",
-  },
-  {
-    type: 'input',
-    name: 'getEmail',
-    message: "What is the team manager's email?",
-  },
-  {
-    type: 'input',
-    name: 'getOfficeNumber',
-    message: "What is the team manager's office number?",
-  },
-  {
-    type: 'list',
-    message: 'What type of team member would you like to add?',
-    name: 'getRole',
-    choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
-  },
-  ];
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "employeeRole",
+        choices: [ "Engineer", "Intern", "I don't want to add any more team members"]
+      },
+];
 
 
-  const intern = [
-    {
-    type: 'input',
-    name: 'getName',
-    message: "What is the team manager's name?",
-  },
-  {
-    type: 'input',
-    name: 'getId',
-    message: "What is the team manager's id?",
-  },
-  {
-    type: 'input',
-    name: 'getEmail',
-    message: "What is the team manager's email?",
-  },
-  {
-    type: 'input',
-    name: 'getOfficeNumber',
-    message: "What is the team manager's office number?",
-  },
-  {
-    type: 'list',
-    message: 'What type of team member would you like to add?',
-    name: 'getRole',
-    choices: ['Engineer', 'Intern', "I don't want to add any more team members"],
-  },
-  ];
-  function createManager(){
-    inquirer.prompt(manager)
-    .then(answers => {
-        console.log(answers)
-        const manager = new Manager(
-            answers.managerName,
-            answers.managerId,
-            answers.managerEmail,
-            answers.managerOfficeNumber,
-       )
-       myTeam.push(manager)
-       createTeamMembers()     
-    })
-    //running prompt for manager questions
-    //using answers from prompt to create manager class
-    //store the created manager in variable
-    //call function for team members created
+function createManager() {
+  inquirer.prompt(manager).then((answers) => {
+    console.log(answers);
+    const manager = new Manager(
+      answers.managerName,
+      answers.managerId,
+      answers.managerEmail,
+      answers.managerOfficeNumber
+    );
+    myTeam.push(manager);
+    createEmployee();
+  });
+  //running prompt for manager questions
+  //using answers from prompt to create manager class
+  //store the created manager in variable
+  //call function for team members created
 }
-    createManager()
+createManager();
 
+
+function createEngineer() {
+    inquirer.prompt(engineer).then((answers) => {
+        console.log(answers);
+        const engineer = new Engineer(
+            answers.engineerName,
+            answers.engineerId,
+            answers.engineerEmail,
+            answers.engineerGitHub
+            );
+            myTeam.push(engineer);
+            createEmployee();
+        });
+        //running prompt for engineer questions
+        //using answers from prompt create engineer class
+        //store created engineer in variable
+        //call function for createTeamMember
+    }
+    createEngineer();
     
-function createTeamMembers(){
-    //running prompt for team questions
-    //for engineer call create engineer function
-    //for intern call intern function
-    //for done run build team function
-}
+    function createIntern() {
+        inquirer.prompt(intern).then((answers) => {
+            console.log(answers);
+            const intern = new Intern(
+                answers.internName,
+                answers.internId,
+                answers.internEmail,
+                answers.internSchool
+                );
+                myTeam.push(intern);
+                createEmployee();
+            });
+            //running prompt for intern questions
+            //using answers from prompt create intern class
+            //store created intern in variable
+            //call function for createTeamMember
+        }
+        createIntern();
+        
+        function createEmployee() {
+            inquirer.prompt(employee).then((answers) => {
+                console.log(answers);
+                const employee = new Employee(
+                    answers.employeeRole,
+                    );
+                    myTeam.push(employee);
+                createEngineer();
+                createIntern();
+                });
+          //running prompt for team questions
+          //for engineer call create engineer function
+          //for intern call intern function
+          //for done run build team function
+        }
+        createTeam();
 
-  // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateHTML(data), err =>{
-        if (err)throw new Error('THERE WAS AN ERROR');
+        // function team() {
+            //call writetofile function with variable
+            function writeToFile(fileName, data) {
+                fs.writeFile(fileName, generateHTML(data), (err) => {
+                    if (err) throw new Error("THERE WAS AN ERROR");
+                    
+                    console.log("Generating HTML");
+                });
+            }
+        // }
 
-        console.log("Generating HTML")
-    });
-}
-
-// TODO: Create a function to initialize app
-
-function init() {
-    inquirer.prompt(engineer)
-    .then(answers => {
-        writeToFile('index.html', answers)
-        console.log(answers)
-    })
-}
-
-function init() {
-    inquirer.prompt(intern)
-    .then(answers => {
-        writeToFile('index.html', answers)
-        console.log(answers)
-    })
-}
-
-
-
+        // function team();
+        
+        function init() {
+            inquirer.prompt(engineer).then((answers) => {
+                writeToFile("index.html", answers);
+                console.log(answers);
+            });
+        }
+        
+    
